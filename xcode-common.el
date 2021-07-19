06bebc3ca8060a5623ee87e37c59d949f88734ae
Xu Xue <juniorxxue@gmail.com>
2021-07-19 23:44:44 +0800

    add support for company

--- xcode-common.el
+++ xcode-common.el
@@ -19,10 +19,10 @@
         (normal-fg        (if (eq variant 'dark) (if (true-color-p) "#b2b2b2" "#b2b2b2") (if (true-color-p) "#262626" "#5f5f87")))
         (normal-bg        (if (eq variant 'dark) (if (true-color-p) "#b2b2b2" "#b2b2b2") (if (true-color-p) "#ffffff" "#5f5f87")))
         (highlight-bg     (if (eq variant 'dark) (if (true-color-p) "#b2b2b2" "#b2b2b2") (if (true-color-p) "#ecf5ff" "#5f5f87")))
-        (select-bg        (if (eq variant 'dark) (if (true-color-p) "#b2b2b2" "#b2b2b2") (if (true-color-p) "#B2D7FF" "#5f5f87")))
+        (select-bg        (if (eq variant 'dark) (if (true-color-p) "#b2b2b2" "#b2b2b2") (if (true-color-p) "#b4d8fd" "#5f5f87")))
         (comment-fg       (if (eq variant 'dark) (if (true-color-p) "#b2b2b2" "#b2b2b2") (if (true-color-p) "#8a99a6" "#5f5f87")))
         (keyword-fg       (if (eq variant 'dark) (if (true-color-p) "#b2b2b2" "#b2b2b2") (if (true-color-p) "#ad3da4" "#5f5f87")))
-        (function-use-fg  (if (eq variant 'dark) (if (true-color-p) "#b2b2b2" "#b2b2b2") (if (true-color-p) "#3e8087" "#5f5f87")))
+        (function-use-fg  (if (eq variant 'dark) (if (true-color-p) "#b2b2b2" "#b2b2b2") (if (true-color-p) "#4b21b0" "#5f5f87")))
         (function-def-fg  (if (eq variant 'dark) (if (true-color-p) "#b2b2b2" "#b2b2b2") (if (true-color-p) "#0f68a0" "#5f5f87")))
         (error-fg         (if (eq variant 'dark) (if (true-color-p) "#b2b2b2" "#b2b2b2") (if (true-color-p) "#d12f1b" "#5f5f87")))
         (cursor-bg        (if (eq variant 'dark) (if (true-color-p) "#b2b2b2" "#b2b2b2") (if (true-color-p) "#262626" "#5f5f87")))
@@ -33,7 +33,10 @@
         (var-fg           (if (eq variant 'dark) (if (true-color-p) "#b2b2b2" "#b2b2b2") (if (true-color-p) "#262626" "#5f5f87")))
         (warning-fg       (if (eq variant 'dark) (if (true-color-p) "#b2b2b2" "#b2b2b2") (if (true-color-p) "#78492a" "#5f5f87")))
         (match-fg         (if (eq variant 'dark) (if (true-color-p) "#b2b2b2" "#b2b2b2") (if (true-color-p) "#262626" "#5f5f87")))
-        (border-fg        (if (eq variant 'dark) (if (true-color-p) "#b2b2b2" "#b2b2b2") (if (true-color-p) "#b2b2b2" "#5f5f87")))
+        (border-fg        (if (eq variant 'dark) (if (true-color-p) "#b2b2b2" "#b2b2b2") (if (true-color-p) "#ffffff" "#5f5f87")))
+        (modeline-bg      (if (eq variant 'dark) (if (true-color-p) "#b2b2b2" "#b2b2b2") (if (true-color-p) "#e5e5e5" "#5f5f87")))
+        (tooltip-bg       (if (eq variant 'dark) (if (true-color-p) "#b2b2b2" "#b2b2b2") (if (true-color-p) "#f4f4f4" "#5f5f87")))
+        (tooltip-selection-bg       (if (eq variant 'dark) (if (true-color-p) "#b2b2b2" "#b2b2b2") (if (true-color-p) "#b4d8fd" "#5f5f87")))
         )
 
     (cl-loop for (cvar . val) in xcode-theme-custom-colors
@@ -48,12 +51,12 @@
      `(default ((,class (:background ,normal-bg :foreground ,normal-fg))))
      `(default-italic ((,class (:italic t))))
      `(error ((,class (:foreground ,error-fg))))
-     `(font-lock-builtin-face ((,class (:foreground ,keyword-fg))))
+     `(font-lock-builtin-face ((,class (:foreground ,keyword-fg :weight semibold))))
      `(font-lock-comment-face ((,class (:foreground ,comment-fg))))
      `(font-lock-constant-face ((,class (:foreground ,number-fg))))
      `(font-lock-doc-face ((,class (:foreground ,comment-fg))))
      `(font-lock-function-name-face ((,class (:foreground ,function-def-fg))))
-     `(font-lock-keyword-face ((,class (:foreground ,keyword-fg))))
+     `(font-lock-keyword-face ((,class (:foreground ,keyword-fg :weight semibold))))
      `(font-lock-negation-char-face ((,class (:foreground ,character-fg))))
      `(font-lock-preprocessor-face ((,class (:foreground ,function-def-fg))))
      `(font-lock-reference-face ((,class (:foreground ,number-fg))))
@@ -71,12 +74,30 @@
      `(minibuffer-prompt ((,class (:inherit bold :foreground ,keyword-fg))))
      `(warning ((,class (:foreground ,warning-fg))))
      `(vertical-border ((,class (:foreground ,border-fg))))
+     `(region ((,class (:background ,highlight-bg :extend t))))
 
 ;;;;; mode-line
-     `(mode-line           ((,class (:foreground ,normal-fg :background ,normal-bg :box (:color ,border-fg :line-width 1)))))
+     `(mode-line           ((,class (:foreground ,normal-fg :background ,modeline-bg :box (:color ,border-fg :line-width 1)))))
      `(mode-line-buffer-id ((,class (:foreground ,function-use-fg))))
-     `(mode-line-inactive  ((,class (:foreground ,normal-fg :background ,normal-bg  :box (:color ,border-fg :line-width 1)))))
-     
+     `(mode-line-inactive  ((,class (:foreground ,normal-fg :background ,modeline-bg  :box (:color ,border-fg :line-width 1)))))
+
+;;;;; mood-line
+     `(mood-line-buffer-name ((,class (:foreground ,normal-fg))))
+     `(mood-line-major-mode ((,class (:foreground ,normal-fg))))
+
+;;;;; company-mode
+     `(company-template-field ((,class (:inherit region))))
+     `(company-tooltip ((,class (:background ,tooltip-bg :foreground ,normal-fg))))
+     `(company-tooltip-common ((,class (:background ,tooltip-bg :foreground ,normal-fg))))
+     `(company-tooltip-common-selection ((,class (:foreground ,normal-fg))))
+     `(company-tooltip-selection ((,class (:background ,tooltip-selection-bg :foreground ,normal-fg))))
+     `(company-scrollbar-bg ((,class (:background ,tooltip-bg))))
+     `(company-scrollbar-fg ((,class (:background ,modeline-bg))))
+     `(company-tooltip-annotation ((,class (:foreground ,comment-fg))))
+
+;;;;; company-coq
+    `(company-coq-snippet-hole-face ((,class (:weight light :italic nil)))) 
+
 ;;;;; proof-general
      `(proof-locked-face ((,class (:background ,highlight-bg))))
      `(coq-solve-tactics-face  ((,class (:foreground ,function-use-fg))))
